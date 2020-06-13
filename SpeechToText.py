f640d6d2d5ce22b1ef87ae29af6565075c389008
Saber Nomen <genericp3rson@gmail.com>
2020-06-12 19:38:34 -0500

    Integration

--- SpeechToText.py
+++ SpeechToText.py
@@ -11,6 +11,7 @@ def SpeakText(command):
     engine.say(command)  
     engine.runAndWait() 
     
+addTab = 0
 while(1):     
     try: 
         with sr.Microphone() as source2: 
@@ -25,6 +26,48 @@ while(1):
                 break
 
             print(MyText) 
+            word_to_num = {"one": 1, "two": 2, "three": 3, "four": 4,
+                           "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}
+
+            words = MyText.split()
+            response = ""
+            print(words)
+            if words[0] == "back" and words[1] == "tab":
+                response = ""
+                addTab -= 1
+            elif words[0] == "print" and words[1] == "string":
+                response = "\t"*addTab + \
+                    "print(\"" + "".join(words[2:]) + "\")"
+            elif words[0] == "print":
+                print(words[1])
+                # print(word_to_num(words[1]))
+                try:
+                    response = "\t"*addTab + \
+                        "print(" + str(word_to_num[words[1]]) + ")"
+                except:
+                    response = "\t"*addTab + \
+                        "print(" + " ".join(words[1:]) + ")"
+            elif words[1] == "equals":
+                try:
+                    response = "\t"*addTab + words[0] + \
+                        " = " + str(word_to_num[words[2]])
+                except:
+                    response = "\t"*addTab + \
+                        words[0] + " = " + " ".join(words[2:])
+            elif words[0] == "if" and words[2] == "equals":
+                words[2] = "=="
+                response = "\t"*addTab + " ".join(words) + ":"
+                addTab += 1
+            elif words[0] == "for":
+                response = "\t"*addTab + " ".join(words) + ":"
+                addTab += 1
+
+
+            # print("\n".join(lines))
+
+            # toParse = "\n".join([x for x in lines if x != ""])
+
+            print(response)
             lines.append(MyText)
     except: 
         pass
@@ -33,8 +76,43 @@ print(lines)
 SpeakText("Done")
 
 
+word_to_num = {"one": 1, "two": 2, "three": 3, "four": 4,
+               "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}
+
+addTab = 0
+for i in range(len(lines)):
+    words = lines[i].split()
+    print(words)
+    if words[0] == "back" and words[1] == "tab":
+        lines[i] = ""
+        addTab -= 1
+    elif words[0] == "print" and words[1] == "string":
+        lines[i] = "\t"*addTab + "print(\"" + "".join(words[2:]) + "\")"
+    elif words[0] == "print":
+        print(words[1])
+        # print(word_to_num(words[1]))
+        try:
+            lines[i] = "\t"*addTab + \
+                "print(" + str(word_to_num[words[1]]) + ")"
+        except:
+            lines[i] = "\t"*addTab + "print(" + " ".join(words[1:]) + ")"
+    elif words[1] == "equals":
+        try:
+            lines[i] = "\t"*addTab + words[0] + \
+                " = " + str(word_to_num[words[2]])
+        except:
+            lines[i] = "\t"*addTab + words[0] + " = " + " ".join(words[2:])
+    elif words[0] == "if" and words[2] == "equals":
+        words[2] = "=="
+        lines[i] = "\t"*addTab + " ".join(words) + ":"
+        addTab += 1
+    elif words[0] == "for":
+        lines[i] = "\t"*addTab + " ".join(words) + ":"
+        addTab += 1
 
 
+# print("\n".join(lines))
 
+toParse = "\n".join([x for x in lines if x != ""])
 
-
+print(toParse)
