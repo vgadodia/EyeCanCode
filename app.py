50b0727a6705475d7d7267a695cc6b99a65e719d
Saber Nomen <genericp3rson@gmail.com>
2020-06-13 15:02:45 -0500

    Integration with JS and bare start of Academy

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 # import SpeechToText
 
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
+# socketio = SocketIO(app)
 CODE = []
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def index():
     return render_template("index.html", code=CODE)
+
+
+@app.route('/', methods=["POST"])
+def index1():
+    global CODE
+    CODE.append(request.form.get("code", False))
+    return render_template("index.html", code=CODE)
+
+
+# @socketio.on('message')
+# def handle_message(message):
+#     print('received message: ' + message)
 
 @app.route('/<string:code>', methods=["POST", "GET"])
 def display(code):
@@ -17,3 +33,4 @@ def display(code):
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+    # socketio.run(app)
---
+++ test.js
@@ -0,0 +1,57 @@
+let inp = "x = list 6 5 2";
+// let words = event.results[i][0].transcript.split(" ");
+let words = inp.split(" ");
+const word_to_num = {
+    "one": 1, "two": 2, "three": 3, "four": 4,
+    "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10
+}
+// console.log(word_to_num["one"]);
+let addTab = 0;
+
+let str = "";
+for (let i = 0; i < addTab; ++i) str+="\t";
+
+if (words[0] == "back" && words[1] == "tab") {
+    addTab--;
+} else if (words[0] == "print" && words[1] == "string") {
+    words.shift();
+    words.shift();
+    str += "print(\"" + words.join(" ") + "\")"
+} else if (words[0] == "print") {
+    if (word_to_num[words[1]]) {
+        str += "print(" + (word_to_num[words[1]]) + ")";
+    } else {
+        words.shift();
+        str += "print(" + words.join(" ") + ")"
+    }
+} else if ((words[1] == "equals" || words[1] == "=") && words[2] == "string") {
+    let first = words.shift();
+    words.shift();
+    words.shift();
+    str+=first + " = " + "'" + words.join(" ") + "'";
+} else if ((words[1] == "equals" || words[1] == "=") && words[2] == "list") {
+    let first = words.shift();
+    words.shift();
+    str += first + " = [";
+    let j = 1;
+    while (j < words.length) {
+        if (words[j] == 'string'){
+            str += "'" + words[j + 1] + "', ";
+            j += 2;
+        }
+        else {
+            str += words[j] + ", ";
+            j ++;
+        }
+    }
+    str = str.substring(0, str.length-2) + "]";
+} else if (words[1] == "equals" || words[1] == "=")  {
+    if (word_to_num[words[2]]) {
+        str+= words[0] + " = " + word_to_num[words[2]];
+    } else {
+        let first = words.shift();
+        words.shift();
+        str+=first + " = "
+    }
+}
+console.log(str);
